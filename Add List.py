d27e336526dc565eb38f6c104f27b33c38d8ea29
Al Sadat Mission <158583660+Alsadat75@users.noreply.github.com>
2024-12-05 22:36:35 +0600

    Add files via upload

---
+++ Add List.py
@@ -0,0 +1,10 @@
+blue=["Samia","Kona","Faria","Tonni"]
+blue[1]="Moyna"
+print(blue)
+# add item by using append
+blue.append("Kalua")
+print(blue)
+
+#add item by using insert
+blue.insert(1,"orrange")
+print(blue)
