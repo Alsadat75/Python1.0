99e45ac269655250f6aafcd99bb26ed17f3fa07b
Al Sadat Mission <158583660+Alsadat75@users.noreply.github.com>
2024-12-05 23:29:48 +0600

    Add files via upload

---
+++ Remove list.py
@@ -0,0 +1,16 @@
+newlist=["Samia","Sunaira","Suba","Sanjida",100,40,50,40]
+newlist.remove("Sanjida")
+print(newlist)
+#using pop
+newlist.pop()
+print(newlist)
+#using del keyword
+del newlist[0]
+print(newlist)
+
+#using clear method
+print(newlist)
+newlist.clear()
+print(newlist)
+
+
