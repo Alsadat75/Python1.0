85e2995a20e7c6ddb8051676ee9de6577cb738bc
Al Sadat Mission <158583660+Alsadat75@users.noreply.github.com>
2024-12-06 20:31:44 +0600

    Add files via upload

---
+++ Loop List.py
@@ -0,0 +1,14 @@
+blue=["Samia","Kona","Faria","Tonni"]
+for sky in blue:
+    print(sky)
+
+    #using rangr len
+
+for sky in range(len(blue)):
+    print(sky)
+
+    #using while loop
+    y=0
+    while y<len(blue):
+        print(blue[y])
+        y=y+1
